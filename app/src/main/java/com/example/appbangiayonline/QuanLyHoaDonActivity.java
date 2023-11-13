5a42b01fd76e01ca530bf3ffaf3ac0c654431f83
Nguyen Tien Anh <anhntph37315@fpt.edu.vn>
2023-11-13 10:43:32 +0700

    tanh - man hinh thong ke va quan ly hoa don

---
+++ app/src/main/java/com/example/appbangiayonline/QuanLyHoaDonActivity.java
@@ -0,0 +1,14 @@
+package com.example.appbangiayonline;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QuanLyHoaDonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quan_ly_hoa_don);
+    }
+}
